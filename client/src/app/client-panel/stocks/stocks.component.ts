bea4394095f91a4987675348a3295ab63481427e
Martin Bechev <martinbechev@gmail.com>
2019-01-26 04:00:44 +0200

    Fill client overview mode - stocks with data from database

--- client/src/app/client-panel/stocks/stocks.component.ts
+++ client/src/app/client-panel/stocks/stocks.component.ts
@@ -1,7 +1,8 @@
+import { StockDTO } from './../../models/stock.dto';
 import { Component, Injectable, OnInit, ViewChild, } from '@angular/core';
 import { StocksService } from '../../core/stocks.service';
 import { AgGridNg2 } from 'ag-grid-angular';
-import { UsersService } from '../../core/user.service';
+import { GridOptions, } from 'ag-grid-community';
 
 @Injectable()
 @Component({
@@ -10,35 +11,48 @@ import { UsersService } from '../../core/user.service';
     styleUrls: ['./stocks.component.css']
 })
 export class StocksComponent implements OnInit {
-    private stockdata = [];
     private name: string;
     @ViewChild('agGrid') agGrid: AgGridNg2;
+    public gridOptions: GridOptions;
     private columnDefs = [
-        { headerName: 'Symbol', field: 'symbol', sortable: true, filter: true, checkboxSelection: true },
-        { headerName: 'Market', field: 'market', sortable: true, filter: true, checkboxSelection: true },
-        { headerName: 'Sell Price', field: 'sellprice', sortable: true, filter: true, checkboxSelection: true },
-        { headerName: 'Buy Price', field: 'buyprice', sortable: true, filter: true, checkboxSelection: true }
+        { headerName: 'Symbol', field: 'symbol', sortable: true, filter: true, checkboxSelection: false },
+        { headerName: 'Market', field: 'market', sortable: true, filter: true, checkboxSelection: false },
+        { headerName: 'Sell Price', field: 'sellprice', sortable: true, filter: true, checkboxSelection: false },
+        { headerName: 'Buy Price', field: 'buyprice', sortable: true, filter: true, checkboxSelection: false }
     ];
-    private rowData = [{
-        symbol: 'BTC',
-        market: 'Bitcoin',
-        sellprice: 20,
-        buyprice: 22,
-    },
-    {
-        symbol: 'ETH',
-        market: 'Ethereum',
-        sellprice: 200,
-        buyprice: 228,
-    }];
+    private rowData = [];
+    private rowSelection = 'single';
+
     constructor(
         private stockService: StocksService,
-    ) {
-    }
+    ) { }
     ngOnInit() {
         this.name = localStorage.getItem('client_name');
-        // this.rowData = this.stockService.getStockData();
+        this.gridOptions = <GridOptions>{
+            enableRangeSelection: true,
+            columnDefs: this.columnDefs,
+            onGridReady: () => {
+                this.stockService.retrieveCompanyPrices().subscribe((response: []) => {
+                    response.forEach((stock: StockDTO) => {
+                        const marketData: any = {};
+                        marketData.symbol = stock.company.abbr;
+                        marketData.market = stock.company.name;
+                        marketData.sellprice = +stock.lowprice;
+                        marketData.buyprice = +stock.highprice;
+                        this.rowData.push(marketData);
+                    });
+                    if (this.gridOptions.api) {
+                        this.gridOptions.api.setRowData(this.rowData);
+                    }
+                });
+                this.gridOptions.rowHeight = 50;
+            }
+        };
     }
+    onRowSelected(event) {
+        console.log(event);
+    }
+    // For select a stock
     getSelectedRows() {
         const selectedNodes = this.agGrid.api.getSelectedNodes();
         const selectedData = selectedNodes.map(node => node.data);
